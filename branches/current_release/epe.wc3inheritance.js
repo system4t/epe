54a64607ccd78972833227cd66b69790115f9ea8
Tavs <no@where.com>
2008-05-01 11:03:11 +0000

    Initial creation of release branch
    git-svn-id: https://svn.system4t.com/epe@2 36e2613f-e23b-425f-a1e4-47cecc24fb9d

---
+++ branches/current_release/epe.wc3inheritance.js
@@ -0,0 +1,34 @@
+
+// If W3C inheritance is enabled
+if (document.createEventObject && window.EPE && EPE.ENABLE_W3C_INHERITANCE) {
+  // Create inheritance from native JS Object
+  // Custom EPE Object object
+  EPE.Object =
+    function() {
+    };
+  
+  // Save ref. to native Object
+  EPE._Object = Object;
+  
+  // Replace with EPE Object
+  Object = EPE.Object;
+  
+  // When pseudo object.prototype is updated this function is run
+  EPE.updateObject =
+    function() {
+      // Copy to real Object constructor
+      EPE._Object.prototype[event.propertyName] = event.srcElement[event.propertyName];
+      // If HTMLCollecations are enabled
+      if (EPE.ENABLE_W3C_INHERITANCE) {
+        // Copy to HTMLCollection constructor
+        HTMLCollection.prototype[event.propertyName] = event.srcElement[event.propertyName];
+      }
+      // Copy to HTMLElement prototype
+      HTMLElement.prototype[event.propertyName] = event.srcElement[event.propertyName];
+    };
+  
+  // Make pseudo object.prototype live and listen for updates
+  Object.prototype = EPE.IECreateElement('epe');
+  document.documentElement.childNodes[0].appendChild(Object.prototype);
+  Object.prototype.attachEvent('onpropertychange',EPE.updateObject);
+}
