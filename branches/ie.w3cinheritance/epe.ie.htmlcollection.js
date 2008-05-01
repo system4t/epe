54d86ea8897bd08f4c3332dd60a92783e7a7a696
Tavs <no@where.com>
2008-05-01 13:05:15 +0000

    Initial branch for IE W3C Inheritance
    git-svn-id: https://svn.system4t.com/epe@11 36e2613f-e23b-425f-a1e4-47cecc24fb9d

---
+++ branches/ie.w3cinheritance/epe.ie.htmlcollection.js
@@ -0,0 +1,30 @@
+// If HTMLCollections are enabled
+if (document.createEventObject && windw.EPE && EPE.ENABLE_W3C_COLLECTION) {
+  /**
+  * HTMLCollection. A collection of element.
+  * @param a {Array} A native IE collection. Not really an array but behaves like one.
+  * @constructor
+  */
+  function HTMLCollection(a) {
+    var l = a.length;
+    for(var i=0; i<l; i++)
+      this[i] = a[i];
+    this.length = a.length;
+  }
+  
+  HTMLCollection._prototype = HTMLCollection.prototype;
+
+  EPE.updateHTMLCollection =
+    function() {
+      //alert(event.srcElement[event.propertyName]);
+      HTMLCollection._prototype[event.propertyName] = event.srcElement[event.propertyName];
+    };
+  /*
+  // Make pseudo object.prototype live and listen for updates
+  HTMLCollection.prototype = EPE.IECreateElement('epe');
+  HTMLCollection.prototype.constructor = HTMLCollection;
+  document.documentElement.childNodes[0].appendChild(HTMLCollection.prototype);
+  HTMLCollection.prototype.attachEvent('onpropertychange',EPE.updateHTMLCollection);
+  a change for testing merge
+  */
+}
