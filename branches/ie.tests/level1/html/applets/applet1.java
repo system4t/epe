721e90f3f5348ea2d6c85ec23d1641fda955f610
Allan <no@where.com>
2008-05-24 10:03:05 +0000

    W3C DOM Level 1 tests converted to Selenese.
    git-svn-id: https://svn.system4t.com/epe@39 36e2613f-e23b-425f-a1e4-47cecc24fb9d

---
+++ branches/ie.tests/level1/html/applets/applet1.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+public class applet1 extends Applet {
+    public void init() {
+    }
+    public void start() {
+    }
+    public void stop() {
+    }
+    public void destroy() {
+    }
+}
---
+++ branches/ie.tests/level1/html/applets/applet2.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+public class applet2 extends Applet {
+    public void init() {
+    }
+    public void start() {
+    }
+    public void stop() {
+    }
+    public void destroy() {
+    }
+}
