07f4cb905f8e5b5db0dab16321dfc20df07772a7
Tavs <no@where.com>
2008-07-05 01:23:20 +0000

    Added IE conditional if statements so code won't be executed by other browsers
    git-svn-id: https://svn.system4t.com/epe@87 36e2613f-e23b-425f-a1e4-47cecc24fb9d

--- trunk/epe.ie.cssfloat.js
+++ trunk/epe.ie.cssfloat.js
@@ -1,17 +1,18 @@
 // Disable the default behavior when dragging an image.
 // Useful if you're using drag'n'drop.
-
-EPE.PlugIn.CssFloat = new EPE.PlugIn();
-EPE.PlugIn.CssFloat.addEPEListener('attach',
-  function() {
-    if (this.style.cssFloat)
-      this.style.styleFloat = this.style.cssFloat;
-  }
-);
-
-EPE.PlugIn.CssFloat.addEPEListener('change',
-  function(e) {
-    if (e.propertyName == 'style.cssFloat')
-      this.style.styleFloat = this.style.cssFloat;
-  }
-);
+if (document.createEventObject) {
+  EPE.PlugIn.CssFloat = new EPE.PlugIn();
+  EPE.PlugIn.CssFloat.addEPEListener('attach',
+    function() {
+      if (this.style.cssFloat)
+        this.style.styleFloat = this.style.cssFloat;
+    }
+  );
+  
+  EPE.PlugIn.CssFloat.addEPEListener('change',
+    function(e) {
+      if (e.propertyName == 'style.cssFloat')
+        this.style.styleFloat = this.style.cssFloat;
+    }
+  );
+}
--- trunk/plugins/epe.ie.imageundragable.js
+++ trunk/plugins/epe.ie.imageundragable.js
@@ -1,10 +1,11 @@
 // Disable the default behavior when dragging an image.
 // Useful if you're using drag'n'drop.
-
-EPE.PlugIn.ImgUndragable = new EPE.PlugIn('img');
-EPE.PlugIn.ImgUndragable.addEPEListener('create',
-  function() {
-    // Bypass UEM
-    this.attachEvent('ondragstart', function(){event.returnValue = false;});
-  }
-);
+if (document.createEventObject) {
+  EPE.PlugIn.ImgUndragable = new EPE.PlugIn('img');
+  EPE.PlugIn.ImgUndragable.addEPEListener('create',
+    function() {
+      // Bypass UEM
+      this.attachEvent('ondragstart', function(){event.returnValue = false;});
+    }
+  );
+}
