f9e03cb7e9bc4fd0bbb4dbdb341c177d08cd725c
Tavs <no@where.com>
2010-04-02 10:11:53 +0000

    Removed auto init
    git-svn-id: https://svn.system4t.com/epe@99 36e2613f-e23b-425f-a1e4-47cecc24fb9d

--- trunk/ie67/epe.js
+++ trunk/ie67/epe.js
@@ -7,14 +7,3 @@ window.EPE = {};
 Save native createElement method
 */
 EPE.IECreateElement = document.createElement;
-
-window.attachEvent('onload',
-  function() {
-    var c = document.getElementsByTagName('*');
-    var l = c.length;
-    for(var i=0; i<l; i++) {
-      var t = c[i].nodeName.toLowerCase();
-      EPE.extend(t, EPE.tags[t] ? EPE.tags[t] : HTMLElement);
-    }
-  }
-);
